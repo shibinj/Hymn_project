b4ba74c70d8a80deba2b8076c9484bb10ca563f8
shibinj <johnshibin@hotmail.com>
2026-08-29 08:03:17 +0530

    security: remove all hardcoded fallback credentials from repo and untrack firebase-config.js

--- admin/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDJ5-YBSIxm7HuZc2D82OkTxxBHNRw6Awk",
-    authDomain: "holy-hymns.firebaseapp.com",
-    projectId: "holy-hymns",
-    storageBucket: "holy-hymns.firebasestorage.app",
-    messagingSenderId: "895486383107",
-    appId: "1:895486383107:web:f2915742618cfd1e02aa36"
-};
-export const ALLOWED_ADMINS = ["shibin.john@gmail.com","abykulathakkal@gmail.com","nehabiju1507@gmail.com"];
